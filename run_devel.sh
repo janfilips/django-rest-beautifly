5a2c9beaff1dda8547a43333e7c15ca904e21b76
Jan Filips <janfilipsgt@gmail.com>
2020-03-08 13:52:56 +0100

    decomissioned run_devel

--- run_devel.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-python3 manage.py makemigrations --settings=config.settings.develop
-python3 manage.py migrate --run-syncdb --settings=config.settings.develop
-python3 manage.py runserver --settings=config.settings.develop 0.0.0.0:8000
